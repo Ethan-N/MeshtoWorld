3a32fa937726592480be706fefc7dc11e35b08dd
Ethan-N <ethannevidomsky@gmail.com>
2019-08-05 18:41:19 -0400

    Mesh seems synced with vive controller

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -28,10 +28,10 @@ void ofApp::setup() {
 
 	flipped = false;
 
-	float diag_fov = 85.0;
-
 	controller.setScale(.010);
 	ezcam.setScale(.010);
+
+	float diag_fov = 70.0;
 
 	image_diag = sqrt(pow(w, 2) + pow(h, 2));
 	ofLog() << "Image Diagonal: " << image_diag;
@@ -39,7 +39,7 @@ void ofApp::setup() {
 	ofLog() << "Vertical FOV: " << vert_fov;
 	horiz_fov = diag_fov * w / image_diag;
 	ofLog() << "Horizontal FOV: " << horiz_fov;
-	focus_len = (image_diag / 2.0) / tan(vert_fov / 2.0);
+	focus_len = (image_diag / 2.0) / tan(vert_fov *3.1415/180.0 / 2.0);
 	ofLog() << "Focus Length: " << focus_len;
 	
 	for (int y = 0; y < h; ++y) {
@@ -82,24 +82,27 @@ void ofApp::update(){
 				if (depth[x + w * y] != 0 && depth[x + w * y] < 8000.0 && depth[x + w * y] == depth[x + w * y]) {
 					float* point = points[(x + w * y)].getPtr();
 
-					float obj_height = (depth[x + w * y]/1000.0 + pixel_focus[x + w * y]) * abs_height[x + w * y] / pixel_focus[x + w * y];
+					float obj_height = (depth[x + w * y]/1000.0 + pixel_focus[x + w * y]) *  abs_height[x + w * y] / pixel_focus[x + w * y];
 					float actual_pixel_base = sqrt(pow(depth[x + w * y]/1000.0 + pixel_focus[x + w * y],2) - pow(obj_height,2)) - pixel_base[x + w * y];
 
 					points[(x + w * y)] = ezcam.getPosition();
-					if(x >= w / 2.0)
-						points[(x + w * y)] += ((w/2.0+cos(pixel_base_ang[x + w * y])) * actual_pixel_base + abs_width[x + w * y]) * ezcam.getXAxis() / 1000.0;
+					if (x >= w / 2.0){
+						float x_val = (cos(pixel_base_ang[x + w * y]) * actual_pixel_base + abs_width[x + w * y]);
+						points[(x + w * y)] += x_val * ezcam.getXAxis() / 1000.0;
+					}
+						//points[(x + w * y)] += (cos(pixel_base_ang[x + w * y]) * actual_pixel_base + abs_width[x + w * y]) * ezcam.getXAxis() / 1000.0;
 						//*point =  ezcam.getX() + (cos(pixel_base_ang[x + w * y]) * actual_pixel_base + abs_width[x + w * y]) / 1000.0;
 					else
-						points[(x + w * y)] += ((w/2.0-cos(pixel_base_ang[x + w * y])) * actual_pixel_base + abs_width[x + w * y]) * ezcam.getXAxis() / 1000.0;
+						points[(x + w * y)] += -(cos(pixel_base_ang[x + w * y]) * actual_pixel_base + abs_width[x + w * y]) * ezcam.getXAxis() / 1000.0;
 						//*point = ezcam.getX() - (cos(pixel_base_ang[x + w * y]) * actual_pixel_base + abs_width[x + w * y]) / 1000.0;
 
 					if (y >= h / 2.0)
-						points[(x + w * y)] += (h/2.0 + obj_height) * ezcam.getYAxis() / 1000.0;
+						points[(x + w * y)] += -obj_height * ezcam.getYAxis() / 1000.0;
 						//*(point+1) = ezcam.getY() +  obj_height / 1000.0;
 					else
-						points[(x + w * y)] += (h/2.0 - obj_height) * ezcam.getYAxis() / 1000.0;
+						points[(x + w * y)] += obj_height * ezcam.getYAxis() / 1000.0;
 						//*(point+1) = ezcam.getY() - obj_height  / 1000.0;
-					points[(x + w * y)] += sin(pixel_base_ang[x + w * y]) * actual_pixel_base * ezcam.getZAxis();
+					points[(x + w * y)] += -sin(pixel_base_ang[x + w * y]) * actual_pixel_base * ezcam.getZAxis();
 					//*(point+2) = ezcam.getZ() + sin(pixel_base_ang[x + w * y]) * actual_pixel_base;
 
 					
@@ -117,41 +120,28 @@ void ofApp::update(){
 
 	fbo.begin();
 	ofClear(0,0,0,255);
+	ofSetColor(255);
 
 	ezcam.setFov(85); 
 	controller.setFov(85);
-	//cam.begin();
 	ezcam.begin();
-	ofPushMatrix();
-	ofTranslate(-ezcam.getX(), -ezcam.getY(), -ezcam.getZ());
 	//vbo.drawElements(GL_POINTS, index);
-	ofPopMatrix();
 	ezcam.end();
-	//cam.end();
 
 	if (flipped) {
 		controller.begin();
-		ezcam.draw();
+		//ezcam.draw();
 		vbo.drawElements(GL_POINTS, index);
 		controller.end();
 	}
 	else{
 		ezcam.begin();
-		controller.draw();
-		//ofPushMatrix();
-		//ofTranslate(-1.0, -1.0);
 		vbo.drawElements(GL_POINTS, index);
-		//ofPopMatrix();
+		ofSetColor(255, 0, 255);
+		controller.draw();
 		ezcam.end();
 	}
 
-
-	//depth_cam.setFov(receiver.getFov());
-	depth_cam.setFov(33.75);
-	depth_cam.begin();
-	circles.draw();
-	depth_cam.end();
-	depth_cam.move(0, .24, -.06);
 	fbo.end();       
 }
 //--------------------------------------------------------------
